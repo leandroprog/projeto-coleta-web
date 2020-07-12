137b39f56b7ac32d7ce7d57c2eb802d9339bbc48
Leandro Rocha <prog.leandro@gmail.com>
2020-07-12 13:06:41 -0300

    criando o html das páginas

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,11 +1,123 @@
-import React from 'react';
-import './styles.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+import "./styles.css";
 
+import logo from "../../assets/logo.svg";
 
 const CreatePoint = () => {
-    return (
-        <h1>Oi</h1>
-    )
-    
-}
+  return (
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta" />
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para home
+        </Link>
+      </header>
+      <form>
+        <h1>
+          Cadastro do
+          <br /> ponto de coleta
+        </h1>
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input
+            //   onChange={handleInputChange}
+              type="text"
+              name="name"
+              id="name"
+            />
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input
+                // onChange={handleInputChange}
+                type="text"
+                name="email"
+                id="email"
+              />
+            </div>
+
+            <div className="field">
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input
+                // onChange={handleInputChange}
+                type="text"
+                name="whatsapp"
+                id="whatsapp"
+              />
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+          </legend>
+{/* 
+          <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={selectedPosition} />
+          </Map> */}
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+
+              <select  name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+               
+              </select>
+            </div>
+
+            <div className="field">
+              <label htmlFor="city">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma cidade</option>
+
+               
+              </select>
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Itens de coleta</h2>
+            <span>Selecione um ou mais itens abaixo</span>
+          </legend>
+
+          <ul className="items-grid">
+           
+              <li
+                // key={item.id}
+                // onClick={() => handleSelectItem(item.id)}
+                // className={selectedItems.includes(item.id) ? "selected" : ""}
+              >
+                <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+                <span>fds</span>
+              </li>
+          
+          </ul>
+        </fieldset>
+
+        <button type="submit">Cadastrar novo ponto de coleta</button>
+      </form>
+    </div>
+  );
+};
 export default CreatePoint;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 
 
+
 //Pages
 import Home from './pages/Home';
 import CreatePoint from './pages/CreatePoint';
