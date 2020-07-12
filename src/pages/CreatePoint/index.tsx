ea8821dfb6ddb64bc144ce9e2a2f1a46d425cc4e
Leandro Rocha <prog.leandro@gmail.com>
2020-07-12 13:27:56 -0300

    Buscando todos os items de coletas da api

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, Marker, TileLayer } from 'react-leaflet';
+import api from '../../services/api';
 
 import "./styles.css";
 
 import logo from "../../assets/logo.svg";
 
+interface IItems {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
 const CreatePoint = () => {
+
+  const [items, setItems] = useState<IItems[]>([]);
+
+  useEffect( () => {
+      api.get('/items').then(result => {
+        setItems(result.data)
+      });
+  }, []);
+
   return (
     <div id="page-create-point">
       <header>
@@ -103,17 +119,18 @@ const CreatePoint = () => {
             <span>Selecione um ou mais itens abaixo</span>
           </legend>
 
+
           <ul className="items-grid">
-           
+            {items.map(item => (
               <li
-                // key={item.id}
+                key={item.id}
                 // onClick={() => handleSelectItem(item.id)}
-                // className={selectedItems.includes(item.id) ? "selected" : ""}
+                // className={selectedItems.includes(item.id) ? 'selected' : ''}
               >
-                <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
-                <span>fds</span>
+                <img src={item.image_url} alt={item.title} />
+                <span>{item.title}</span>
               </li>
-          
+            ))}
           </ul>
         </fieldset>
 
